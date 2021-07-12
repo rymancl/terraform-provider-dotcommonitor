9b8ca21c1b29ce485dad69be86b0867f0d7ba330
rymancl <rymancl@gmail.com>
2021-07-12 15:25:59 -0400

    feat(scheduler): refactoring intervals to be user friendly
    - weekly interval times changing from minutes
    to hour/minute string format
    - excluded time interval times changing from
    epoch to iso8601 string format
    - rename from_minutes to from
    - rename to_minutes to to
    - rename from_unix to from
    - rename to_unit to to
    
    Closes #39

--- dotcommonitor/validators.go
+++ dotcommonitor/validators.go
@@ -3,9 +3,14 @@ package dotcommonitor
 import (
 	"fmt"
 	"strconv"
+	"time"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
+
+//////////////////////////////
+// Group validators
+//////////////////////////////
 
 // groupAddressNumberIsValid ... validates that the given number is:
 //  1) a string type
@@ -63,10 +68,14 @@ func groupAddressCodeIsValid() schema.SchemaValidateFunc {
 	}
 }
 
+//////////////////////////////
+// Scheduler validators
+//////////////////////////////
+
 // detectInvalidSchedulerWeeklyIntervalDays ... detects if a day string is valid to the API
 //  See Weekly_Intervals: https://www.dotcom-monitor.com/wiki/knowledge-base/scheduler-operations/
 func detectInvalidSchedulerWeeklyIntervalDays(days []string) []string {
-	validDays := []string{"Su","Mo","Tu","We","Th","Fr","Sa"}
+	validDays := []string{"Su", "Mo", "Tu", "We", "Th", "Fr", "Sa"}
 	var invalidDays []string
 	for _, item := range days {
 		if !stringInList(validDays, item) {
@@ -74,4 +83,70 @@ func detectInvalidSchedulerWeeklyIntervalDays(days []string) []string {
 		}
 	}
 	return invalidDays
+}
+
+// validateWeeklyIntervalFrom ... ensure weekly interval to is valid
+//  See Weekly_Intervals: https://www.dotcom-monitor.com/wiki/knowledge-base/scheduler-operations/
+func validateWeeklyIntervalFrom() schema.SchemaValidateFunc {
+	return func(i interface{}, k string) (s []string, es []error) {
+		v, ok := i.(string)
+
+		// first validate it is of string type
+		if !ok {
+			es = append(es, fmt.Errorf("expected type of %s to be string", k))
+			return
+		}
+
+		// then verify input string can be parsed into a Duration
+		if d, err := time.ParseDuration(v); err != nil {
+			es = append(es, fmt.Errorf("unable to parse %v as duration, must be in the format of #h#m", v))
+		} else {
+			// then verify minutes are between 0 & 1439
+			mins := int(d.Minutes())
+			if mins < 0 || mins > 1439 {
+				es = append(es, fmt.Errorf("invalid duration of %v - from time must be between 0 & 1439 minutes", mins))
+			}
+		}
+
+		return
+	}
+}
+
+// validateWeeklyIntervalTo ... ensure weekly interval to is valid
+//  See Weekly_Intervals: https://www.dotcom-monitor.com/wiki/knowledge-base/scheduler-operations/
+func validateWeeklyIntervalTo() schema.SchemaValidateFunc {
+	return func(i interface{}, k string) (s []string, es []error) {
+		v, ok := i.(string)
+
+		// first validate it is of string type
+		if !ok {
+			es = append(es, fmt.Errorf("expected type of %s to be string", k))
+			return
+		}
+
+		// then verify input string can be parsed into a Duration
+		if d, err := time.ParseDuration(v); err != nil {
+			es = append(es, fmt.Errorf("unable to parse %v as duration, must be in the format of #h#m", v))
+		} else {
+			// then verify minutes are between 1 & 1440
+			mins := int(d.Minutes())
+			if mins < 0 || mins > 1440 {
+				es = append(es, fmt.Errorf("invalid duration of %v - to time must be between 1 & 1440 minutes", mins))
+			}
+		}
+		return
+	}
+}
+
+// validateExcludedTimeIntervalTimestamp ... ensure excluded time interval is valid
+func validateExcludedTimeIntervalTimestamp(i interface{}, k string) (ws []string, errors []error) {
+	v := i.(string)
+
+	// validate input can be parsed to iso8601
+	_, err := time.Parse(schedulerExcludedTimeIntervalLayout, v)
+	if err != nil {
+		errors = append(errors, fmt.Errorf("%q cannot be parsed as iso8601 Timestamp Format", v))
+	}
+
+	return
 }
